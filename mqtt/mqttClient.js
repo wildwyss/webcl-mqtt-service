6d9c98d51a400db3496ed7d734ee43ab5b9ab5be
Andri Wild <andri.wild@students.fhnw.ch>
2023-01-04 16:35:09 +0100

    add jsDoc to mqttClient

--- mqtt/mqttClient.js
+++ mqtt/mqttClient.js
@@ -2,13 +2,28 @@ import { Paho } from "./lib/paho-mqtt.js";
 
 export { Client }
 
-const Client = (URL, port, topic) =>
+/**
+ * A client to receive data from an MQTT broker.
+ * @param  { !String } URL       - Uniform Resource Locator of the endpoint.
+ * @param  { !Number } port      - The port, on which the broker is available.
+ * @param  { !String } topic     - A topic under which the data are available
+ * @param  { String }  protocol  - The protocol used for the communication. Default is "/mqtt".
+ * @return { Promise<String> }   - If the request was successful a string containing the data, otherwise an error message
+ * @constructor
+ * @example
+ * Client("broker.hivemq.com", 8000, "test")
+ *    .then(  restDevArray => {
+ *      restDevArray = JSON.parse(restDevArray.toString());
+ *      const devs = restDevArray.map( toDeveloper(imagePath) );
+ *      withDevelopers(devs);
+ *    })
+ *    .catch( err => console.error(err));
+ */
+const Client = (URL, port, topic, protocol = "/mqtt") =>
 
    new Promise((resolve, reject) => {
 
-    const mqttClient = new Paho.MQTT.Client(URL, port, "/mqtt", "clientId_" + Math.random() * 100);
-
-    //mqttClient.startTrace();
+    const mqttClient = new Paho.MQTT.Client(URL, port, protocol, "clientId_" + Math.random() * 100);
 
     const onConnect = () => mqttClient.subscribe(topic);
 
